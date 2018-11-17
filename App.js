0867d4ab8aa7d9ac0392e8c9e067817c9d5fbbca
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-11-17 16:31:57 -0300

    Criando styles (Containers e Boxs)

--- App.js
+++ App.js
@@ -4,18 +4,51 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.containerResultado}>
+        <View style={styles.boxEntradaDeNumeros}></View>
+        <View style={styles.boxResultado}></View>
+        <View style={styles.containerOperadores}>
+          <View style={styles.containerNumeros}>
+            <View style={styles.boxNumeros}></View>
+          </View>
+          <View style={styles.boxOperadores}></View>
+        </View>
       </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerResultado: {
+    backgroundColor: '#4286f4',
+    flexDirection: 'column',
+    flex: 1
   },
+  containerOperadores: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff'
+  },
+  containerNumeros: {
+    backgroundColor: '#4286f4',
+    flexDirection: 'column',
+    flex: 1
+  },
+  boxEntradaDeNumeros: {
+    backgroundColor: '#64ce3d',
+    height: 100
+  },
+  boxResultado: {
+    backgroundColor: '#c92249',
+    height: 100
+  },
+  boxNumeros: {
+    backgroundColor: '#a811af',
+    flex: 1
+  },
+  boxOperadores: {
+    backgroundColor: '#aaa817',
+    flex: 0.3
+  }
 });
