110a646b9491f25e6b7d228005d0d2c81959df35
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-11-17 21:02:15 -0300

    React-navigation

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.containerResultado}>
@@ -9,46 +10,76 @@ export default class App extends React.Component {
         <View style={styles.boxResultado}></View>
         <View style={styles.containerOperadores}>
           <View style={styles.containerNumeros}>
-            <View style={styles.boxNumeros}></View>
+            <View style={styles.boxNumeros}>
+              <Button title="/" style={styles.btnOperador} />
+              <Button title="*" style={styles.btnOperador} />
+              <Button title="-" style={styles.btnOperador} />
+              <Button title="+" style={styles.btnOperador} />
+              <Button title="=" style={styles.btnOperador} />
+            </View>
           </View>
           <View style={styles.boxOperadores}></View>
         </View>
       </View>
-
     );
   }
 }
 
 const styles = StyleSheet.create({
+  // RESULTADO
   containerResultado: {
     backgroundColor: '#4286f4',
     flexDirection: 'column',
     flex: 1
   },
-  containerOperadores: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff'
-  },
-  containerNumeros: {
-    backgroundColor: '#4286f4',
-    flexDirection: 'column',
-    flex: 1
+  boxResultado: {
+    backgroundColor: '#c92249',
+    height: 100
   },
   boxEntradaDeNumeros: {
     backgroundColor: '#64ce3d',
     height: 100
   },
-  boxResultado: {
-    backgroundColor: '#c92249',
-    height: 100
+
+  // NÚMEROS
+  containerNumeros: {
+    backgroundColor: '#4286f4',
+    flexDirection: 'column',
+    flex: 1
   },
   boxNumeros: {
     backgroundColor: '#a811af',
+    flex: 1,
+    flexDirection: 'row'
+  },
+  btnNumero: {
+    backgroundColor: '#494bd1',
     flex: 1
+  },
+
+  // OPERADORES
+  containerOperadores: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff'
   },
   boxOperadores: {
     backgroundColor: '#aaa817',
-    flex: 0.3
+    flex: 0.3,
+    flexDirection: 'column'
+  },
+  btnOperador: {
+    backgroundColor: '#494bd1',
+    flex: 1
   }
 });
+
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: App
+  }
+});
+
+export default createAppContainer(AppNavigator);
+
