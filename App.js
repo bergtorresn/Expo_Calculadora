7200449d2ad8724dadff5db48166e24f6d78b1f3
Rosemberg Torres Nunes <bergtorres1@gmail.com>
2018-11-18 14:08:25 -0300

    Navegando para Home

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { 
+  createStackNavigator, 
+  createAppContainer 
+} from "react-navigation";
+
 import HomeScreen from './screens/HomeScreen'
 import CientificaScreen from './screens/CientificaScreen'
 import SimplesScreen from './screens/SimplesScreen'
 
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Cientifica: CientificaScreen,
+    Simples: SimplesScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
 export default class App extends React.Component {
   render() {
     return (
-      <AppStackNavigator />
+      <AppContainer />
     );
   }
 }
 
-const AppStackNavigator = createStackNavigator({
-  Home: HomeScreen,
-  Cientifica: CientificaScreen,
-  Simples: SimplesScreen
-})
+
