057450d9453b78429e9ed9b4378b5123ae167d4b
Rosemberg Torres <rosemberg@opovodigital.com>
2018-11-21 17:19:10 -0300

    _onPressButton - Recendo valor dos botões e calculando a operação

--- screens/SimplesScreen.js
+++ screens/SimplesScreen.js
@@ -7,48 +7,80 @@ import {
 } from 'react-native';
 
 class SimplesScreen extends React.Component {
+
   static navigationOptions = {
     title: 'Calc Simples',
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = { saida: "", entrada: "" };
+
+    this._onPressNumero = this._onPressButton.bind(this);
+  }
+
+  _onPressButton(value) {
+
+    switch (value) {
+      case "AC":
+        this.setState({
+          entrada: ""
+        });
+        break;
+      case "=":
+        var resultado = eval(this.state.entrada);
+        this.setState({
+          saida: resultado
+        }, function () {
+          console.log(this.state.saida);
+        });
+        break;
+      default:
+        var equacao = this.state.entrada + value;
+        this.setState({
+          entrada: equacao
+        }, function () {
+          console.log(this.state.entrada);
+        });
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.containerEntradaDeOperacoes}>
-          <Text>Entrada de operacoes</Text>
+          <Text>{this.state.entrada}</Text>
         </View>
         <View style={styles.containerResultado}>
-          <Text>Resultado das operacoes</Text>
+          <Text>{this.state.saida}</Text>
         </View>
         <View style={styles.containerOperadoresEBotoes}>
           <View style={styles.containerRow5}>
-            <Button title="AC" />
+            <Button title="AC" onPress={() => this._onPressButton('AC')} />
           </View>
           <View style={styles.containerRow4}>
-            <Button title="7" />
-            <Button title="8" />
-            <Button title="9" />
-            <Button title="/" />
+            <Button title="7" onPress={() => this._onPressButton(7)} />
+            <Button title="8" onPress={() => this._onPressButton(8)} />
+            <Button title="9" onPress={() => this._onPressButton(9)} />
+            <Button title="/" onPress={() => this._onPressButton('/')} />
           </View>
           <View style={styles.containerRow3}>
-            <Button title="4" />
-            <Button title="5" />
-            <Button title="6" />
-            <Button title="*" />
+            <Button title="4" onPress={() => this._onPressButton(4)} />
+            <Button title="5" onPress={() => this._onPressButton(5)} />
+            <Button title="6" onPress={() => this._onPressButton(6)} />
+            <Button title="*" onPress={() => this._onPressButton('*')} />
           </View>
           <View style={styles.containerRow2}>
-            <Button title="1" />
-            <Button title="2" />
-            <Button title="3" />
-            <Button title="+" />
+            <Button title="1" onPress={() => this._onPressButton(1)} />
+            <Button title="2" onPress={() => this._onPressButton(2)} />
+            <Button title="3" onPress={() => this._onPressButton(3)} />
+            <Button title="+" onPress={() => this._onPressButton('+')} />
           </View>
           <View style={styles.containerRow1}>
-            <Button title="0" />
-            <Button title="." />
-            <Button title="-" />
-          </View>
-          <View style={styles.containerRow0}>
-            <Button title="=" />
+            <Button title="0" onPress={() => this._onPressButton(0)} />
+            <Button title="-" onPress={() => this._onPressButton('-')} />
+            <Button title="=" onPress={() => this._onPressButton('=')} />
           </View>
         </View>
       </View>
