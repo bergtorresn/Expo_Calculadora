8226a8828d157a1472e9ed64f4717e06cd3fb85a
Rosemberg Torres <rosemberg@opovodigital.com>
2018-11-21 18:37:40 -0300

    Unificando containerRow

--- screens/SimplesScreen.js
+++ screens/SimplesScreen.js
@@ -54,33 +54,33 @@ class SimplesScreen extends React.Component {
           <Text style={styles.txt}>{this.state.saida}</Text>
         </View>
         <View style={styles.containerOperadoresEBotoes}>
-          <View style={styles.containerRow5}>
+          <View style={styles.containerRow}>
             <Button title="AC" onPress={() => this._onPressButton('AC')} />
           </View>
-          <View style={styles.containerRow4}>
+          <View style={styles.containerRow}>
             <Button title="7" onPress={() => this._onPressButton(7)} />
             <Button title="8" onPress={() => this._onPressButton(8)} />
             <Button title="9" onPress={() => this._onPressButton(9)} />
             <Button title="/" onPress={() => this._onPressButton('/')} />
           </View>
-          <View style={styles.containerRow3}>
+          <View style={styles.containerRow}>
             <Button title="4" onPress={() => this._onPressButton(4)} />
             <Button title="5" onPress={() => this._onPressButton(5)} />
             <Button title="6" onPress={() => this._onPressButton(6)} />
             <Button title="*" onPress={() => this._onPressButton('*')} />
           </View>
-          <View style={styles.containerRow2}>
+          <View style={styles.containerRow}>
             <Button title="1" onPress={() => this._onPressButton(1)} />
             <Button title="2" onPress={() => this._onPressButton(2)} />
             <Button title="3" onPress={() => this._onPressButton(3)} />
             <Button title="+" onPress={() => this._onPressButton('+')} />
           </View>
-          <View style={styles.containerRow1}>
+          <View style={styles.btn} style={styles.containerRow}>
             <Button title="0" onPress={() => this._onPressButton(0)} />
-            <Button title="-" onPress={() => this._onPressButton('-')} />
             <Button title="." onPress={() => this._onPressButton('.')} />
+            <Button title="-" onPress={() => this._onPressButton('-')} />
           </View>
-          <View style={styles.containerRow0}>
+          <View style={styles.containerRow}>
             <Button title="=" onPress={() => this._onPressButton('=')} />
           </View>
         </View>
@@ -97,6 +97,9 @@ const styles = StyleSheet.create({
     marginRight: 20,
     fontWeight: 'bold',
     fontSize: 20
+  },
+  btn: {
+    flex: 1
   },
   container: {
     flex: 1,
@@ -118,46 +121,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column'
   },
-  containerRow5: {
+  containerRow: {
     flexDirection: 'row',
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: "#ffa154" // laranja
-  },
-  containerRow4: {
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: "#852ecc" // roxo
-  },
-  containerRow3: {
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: "#2dcc75" // verde
-  },
-  containerRow2: {
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: "#aa19b5" // rosa
-  },
-  containerRow1: {
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: "#d8d83a" // amarelo
-  },
-  containerRow0: {
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: "#565656" // cinza
   }
 });
